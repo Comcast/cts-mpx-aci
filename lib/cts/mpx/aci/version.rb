273e8ee8a044b5b21691f882bfb9f27a7224eebc
Ernie Brodeur <ebrodeur@ujami.net>
2019-01-14 14:56:12 -0800

    version bump

--- lib/cts/mpx/aci/version.rb
+++ lib/cts/mpx/aci/version.rb
@@ -1,7 +1,7 @@
 module Cts
   module Mpx
     module Aci
-      VERSION = "2.0.0".freeze # Current version of Cts::Mpx::ACI
+      VERSION = "2.0.1".freeze # Current version of Cts::Mpx::ACI
     end
   end
 end
